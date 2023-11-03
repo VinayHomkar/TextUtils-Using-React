01f5538ee6b8c6ab30e86d8216a25fbe2e81942e
Vinay Homkar <homkar2015@gmail.com>
2023-11-03 19:52:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+/*import logo from './logo.svg';*/
 import './App.css';
+import AboutUs from './components/AboutUs';
+import Navbar from './components/Navbar';
+import Alert from './components/Alert'
+import Textform from './components/TextForm';
+import {useState} from 'react';
+
 
 function App() {
+  // eslint-disable-next-line no-unused-vars
+  const [mode, setMode]= useState('light'); //Whether Darkmode Is Enabled Or Not
+  const [alert, setAlert]= useState(null);
+
+  //const [useTheme, setTheme]= useState('');
+  const showAlert =(message, type)=>{
+    setAlert({
+
+      msg :message,
+      type :type
+    })
+    setTimeout(()=>{
+      setAlert(null);
+
+    }, 3000);
+  }
+  /*const radioBtn = ()=>{
+    if (useTheme === 'light'){
+      setTheme('green')
+      document.body.style.backgroundColor =  '#0bf589';
+    }
+  }*/
+  const toggleMode = ()=>{
+  if (mode === 'light'){
+    setMode ('dark')
+    document.body.style.backgroundColor = '#171934'
+    showAlert("Dark Mode Has Been Enabled", "success");
+  }
+  else{
+    setMode ('light')
+    document.body.style.backgroundColor = 'white'
+    showAlert("Light Mode Has Been Enabled", "success");
+  }
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    
+      {<Navbar title = "TextUtils" about = "AboutUs" mode={mode} toggleMode={toggleMode} /*useTheme={useTheme} radioBtn={radioBtn} *//>}
+      {/*<Navbar/>  - Without passing any props */}
+      <Alert alert={alert}/>
+      <div className="container">
+        <Textform heading = "Enter The Text To Analyze :" mode={mode} showAlert={showAlert}/>
+        <AboutUs  mode={mode}/>
+          
+       
+
+      </div>
+    
+    </>
+    );
 }
 
 export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,69 @@
+import React, {useState} from 'react'
+import propTypes from 'prop-types'
+export default function Textform(props) {
+  const handleUPClick = ()=>{
+    //console.log("UpperCase Was Clicked", text)
+    let newText = text.toUpperCase();
+    setText(newText)
+    props.showAlert('Converted To UpperCase','success');
+  }
+  const handleLOClick = ()=>{
+      //console.log("LowerCase Was Clicked", text)
+      let newText = text.toLowerCase();
+      setText(newText)
+      props.showAlert('Converted To LowerCase','success');
+  }
+  const handleClear = ()=>{
+    //console.log("Cleared Text", text)
+    let newText = "";
+    setText(newText)
+    props.showAlert('TeaxtArea Cleared','success');
+  }
+  const handleExtraSpace =()=>{
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "))
+    props.showAlert('Removed Extraa Spaces','success');
+  }
+  function countWords() {
+    var matches = text.match(/[\w\d'-]+/gi);
+    return matches ? matches.length : 0;
+  }
+  
+  
+  
+  //Handle o chnage is not necessary but for clearing console and to count changes of existing string we used and 
+  // if we didnt use handle on chnage function it will not allow to enter text in text area box
+  const handleOnChange = ()=>{
+    console.log("On Change")
+    // eslint-disable-next-line no-restricted-globals
+    setText(event.target.value);
+  }
+
+  const [text, setText] = useState('Vinay');
+    //text = "New Text" //Wrong Way To Chnage The Text
+    //setText("New Text"); //Correct Way To Chnage The Text
+  return (
+    <>
+    <div className="container" style={{color: props.mode === 'dark'?'white':'black'}}>
+      <h2>{props.heading}</h2>
+      <div className="mb-3">
+        <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark'?'grey':'white', color: props.mode === 'dark'?'white':'black'}} id="myBox" rows="10"></textarea>
+      </div>
+      <button className="btn btn-primary mx-2" onClick={handleUPClick}>Convert To UppeCase</button>
+      <button className="btn btn-primary mx-2" onClick={handleLOClick}>Covert To LowerCase</button>
+      <button className="btn btn-primary mx-2" onClick={handleClear}>Clear</button>
+      <button className="btn btn-primary mx-2" onClick={handleExtraSpace}>Clear Spaces</button>
+      
+    </div>
+
+    <div className='container my-3' style={{color: props.mode === 'dark'?'white':'black'}}>
+      <h2>Your Text Summary</h2>
+      <p>{countWords('text')} Words And {text.length} Characters</p>
+      <p>Minutes To Read, SLOW :{0.008 * text.split(" ").length}Minutes, AVERAGE :{0.003333 * text.split(" ").length}Minutes</p>
+      <p>Sentences :{text.split(".").length}</p>
+    </div>
+    </>
+  )
+}
+Textform.propTypes = {heading: propTypes.string}
+
